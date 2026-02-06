773228f848e3efe2659d101ef17921a63eea2942
xevansz <minithbmatthew@gmail.com>
2026-02-06 16:17:39 +0530

    add: mic, cam and screen share permission
    something happened

--- src/webenginehelper.h
+++ src/webenginehelper.h
@@ -8,16 +8,15 @@ class QWebEngineView;
 class QWebEngineProfile;
 class QWebEngineDownloadRequest;
 
-class WebEngineHelper : public QObject
-{
+class WebEngineHelper : public QObject {
     Q_OBJECT
 public:
-    explicit WebEngineHelper(QWebEngineView *view,
-                             ConfigManager *config,
-                             QObject *parent = nullptr);
+    explicit WebEngineHelper(QWebEngineView* view,
+        ConfigManager* config,
+        QObject* parent = nullptr);
 
     void initialize();
-    QWebEngineProfile *profile() const;
+    QWebEngineProfile* profile() const;
     void setAudioMuted(bool muted);
 
 signals:
@@ -26,11 +25,11 @@ signals:
     void activationRequested();
 
 private slots:
-    void handleDownloadRequested(QWebEngineDownloadRequest *download);
-    void handleTitleChanged(const QString &title);
+    void handleDownloadRequested(QWebEngineDownloadRequest* download);
+    void handleTitleChanged(const QString& title);
 
 private:
-    QWebEngineView *m_view;
-    QWebEngineProfile *m_profile;
-    ConfigManager *m_config;
+    QWebEngineView* m_view;
+    QWebEngineProfile* m_profile;
+    ConfigManager* m_config;
 };
